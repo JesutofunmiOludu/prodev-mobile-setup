8b7a1c3541243a91f73520a95cda52a0338ea493
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2026-01-20 20:46:37 +0100

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,76 +7,37 @@ import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-        <ThemedView style={styles.titleContainer}>
-        {/* Added the required Entry Screen string here */}
-        <ThemedText type="title">Entry Screen - Awesome</ThemedText>
-        <HelloWave />
-      </ThemedView>
-
-
-      <ThemedView style={styles.titleContainer}>
-        {/* Requirement: First App Created */}
-        <ThemedText type="title">First App Created</ThemedText>
-        <HelloWave />
-      </ThemedView>
-
-      {/* Your Requested Section */}
-      <View style={styles.customContainer}>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
-<ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+  <Text style={styles.smallText}>ALX is awesome</Text>
+</View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-  // Custom styles for your text
-  customContainer: {
-    paddingVertical: 15,
-    gap: 10,
+  container: {
+    backgroundColor: "#90caf9",
   },
   largeText: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
   },
   mediumText: {
-    fontSize: 18,
-    fontWeight: '600',
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
   },
   smallText: {
-    fontSize: 14,
-    color: '#666',
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
   },
 });
